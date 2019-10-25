eaea33d8c7071aca1e1a35002e4998e8d6f95967
vietanh <hoangvanvietanh@gmail.com>
2019-10-25 21:39:02 +0700

    change link to heroku

--- models/Exam.js
+++ models/Exam.js
@@ -1,5 +1,5 @@
-//var Dia_chi_Dich_vu = "https://dv-webtracnghiem.herokuapp.com/"
-var Dia_chi_Dich_vu = "http://localhost:1200"
+var Dia_chi_Dich_vu = "https://dv-webtracnghiem.herokuapp.com/"
+//var Dia_chi_Dich_vu = "http://localhost:1200"
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function Doc_Danh_sach_De_thi() {
