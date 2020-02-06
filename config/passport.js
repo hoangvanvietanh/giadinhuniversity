9aba66e829da6c1a14500a0400194792baecb4d2
vietanh <hoangvanvietanh@gmail.com>
2020-02-06 16:49:38 +0700

    fix double user when login with facebook

--- config/passport.js
+++ config/passport.js
@@ -81,6 +81,7 @@ module.exports = function (passport) {
         })
         if (checkAddUser == 0) {
           Them_Sinh_vien(user)
+          ListStudents.push(user);
           return done(null, user);
         } else {
           return done(null, user);
